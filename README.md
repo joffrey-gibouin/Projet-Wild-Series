482c56167b89bc9c9b9176fb67a81d4fd1708460
joffrey-gibouin <joffreygibouin@gmail.com>
2021-11-21 22:21:07 +0100

    add Readme with color and Typo

---
+++ README.md
@@ -0,0 +1,14 @@
+LOCAL
+# Projet-Wild-Series
+
+## Colors
+
+* Primary color : #000000
+* Secondary color : #FFE81F
+* Ternary color : #FFFFFF
+
+## Typography
+
+* Title : Distant-Galaxy-fontfacekit
+* Paragraphs: Roboto Slab
+
---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+<h1>Wild Series</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum laudantium nulla officiis quo temporibus! Eligendi enim, error exercitationem, ipsam ipsum minima, molestiae nobis quasi quo sit sunt tempora voluptate. Eos.</p>
+
+
+
+</body>
+</html>
